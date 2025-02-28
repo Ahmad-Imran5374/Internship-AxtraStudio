35df508b74ac054317fb63deb41c6021ded448d7
Ahmad-Imran5374 <ahmadcancerian3@gmail.com>
2025-02-28 19:30:27 +0500

    Tailwind part 1

--- 11-fast-react-pizza/src/ui/Header.jsx
+++ 11-fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import SearchOrder from '../features/order/searchOrder'
+import Username from '../features/user/Username'
 function Header() {
   return (
-    <header>
-        <Link to='/'>Fast React Pizza Co.</Link>
+    <header className='flex items-center justify-between bg-yellow-400 uppercase px-4 py-3 border-b border-stone-200 sm:px-6'>
+        <Link to='/' className='tracking-widest'>Fast React Pizza Co.</Link>
         <SearchOrder/>
-        <p>Ahmad Imran</p>
+        <Username/>
     </header>
   )
 }
